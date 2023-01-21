60ac61b4da409fec4d044b865843afbecb903028
satishcompany <113226680+satishcompany@users.noreply.github.com>
2023-01-21 10:22:10 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for Satish Technologies";
+        return "DevOps Project for Nanda Technologies";
     }
 }
